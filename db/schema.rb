aa0ac891485dfdcef1e3ddc357d956b497bcf1a6
Kimata <kkimata@test.com>
2017-03-10 18:23:28 +0000

    User model push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310180032) do
+ActiveRecord::Schema.define(version: 20170310181816) do
 
   create_table "users", force: :cascade do |t|
     t.string   "mail"
