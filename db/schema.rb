2af09d907e338120b30dffb69d798b814e86d05b
Chiakiene <axt.soccer.11@gmail.com>
2017-03-11 10:12:03 +0000

    add 1 column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311092205) do
+ActiveRecord::Schema.define(version: 20170311101023) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170311092205) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
