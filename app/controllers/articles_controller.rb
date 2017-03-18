58fddde6e5701fe5dfbd5afaca39cb003ed4abfe
Kimata <kkimata@test.com>
2017-03-18 11:42:19 +0000

    test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,15 @@
+require 'nokogiri'
+
 class ArticlesController < ApplicationController
+
+	def clawl
+		clawl_mainichi
+	end
+
+	def clawl_mainichi
+		url = 'http://mainichi.jp/'
+		@clawl_mainichi = Nokogiri::HTML.parse(open(url), nil, nil)
+		@entry_list = @clawl_mainichi.xpath('//ul[@class="list-typeA"]').xpath('.//a')
+	end
+
 end
