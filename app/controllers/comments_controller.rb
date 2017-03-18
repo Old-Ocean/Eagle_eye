8be77d87674b614826e9362578555fcbe3586fa7
Chiakiene <axt.soccer.11@gmail.com>
2017-03-18 11:42:26 +0000

    added some actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+	def create
+		@comment.
+		
+	end
+
+	def index
+		@comments = Comment.all
+	end
+
+	def show
+		@comment = Comment.find(params[:id])
+	end
+
+	def edit
+		
+	end
+
+	def destroy
+		
+	end
+
 end
