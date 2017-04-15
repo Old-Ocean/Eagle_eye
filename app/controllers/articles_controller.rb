f4e57eb6b17686fc8d0aa99243c89cf893e6ff8c
Kimata <kkimata@test.com>
2017-04-15 12:13:18 +0000

    bootstrap追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,9 @@ class ArticlesController < ApplicationController
 		@article = Article.all
 	end
 
-	# def show
-	# 	@article = Article.find(params[:id])
-	# end
+	def show
+		@article = Article.find(params[:id])
+	end
 
 	def clawl
 		clawl_news
@@ -17,7 +17,9 @@ class ArticlesController < ApplicationController
 	def clawl_news
 		url = 'https://mainichi.jp/'
 		@clawl_news = Nokogiri::HTML.parse(open(url), nil, nil)
-		@entry_list = @clawl_news.xpath('//ul[@class="list-typeA"]').xpath('.//a')
+		@entry_list = @clawl_news.xpath('//ul[@class="list-typeA"]').xpath('.//li')
 		@entry_img = @clawl_news.xpath('//ul[@class="list-typeA"]').xpath('.//img')
 	end
+
+
 end
