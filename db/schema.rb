d3673fb4930ca381b11b8ee1c29429cb4daaf437
yamasou <souichirou19960210@gamil.com>
2017-04-20 11:30:35 +0000

    add comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415115835) do
+ActiveRecord::Schema.define(version: 20170420111242) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -24,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170415115835) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "article_id"
   end
 
   create_table "likes", force: :cascade do |t|
